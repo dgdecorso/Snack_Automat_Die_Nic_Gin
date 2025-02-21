ebc2333f44862783336568d685d32af9d27d2746
Diego De Corso <diego.decorso@edu.tbz.ch>
2025-02-21 09:32:08 +0100

    add default project version after gitignore

---
+++ Kasten.java
@@ -0,0 +1,10 @@
+public class Kasten {
+
+    private void snack(){
+        System.out.println("texttest");
+    }
+
+
+
+
+}
---
+++ Main.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
+        window.setResizable(false);
+        window.setTitle("Snackautomat");
+        window.setBackground(Color. white);
+
+        SnackPanel sp = new SnackPanel();
+        window.add(sp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        sp.setupMachine();
+        sp.startMachineThread();
+
+
+
+    }
+}
