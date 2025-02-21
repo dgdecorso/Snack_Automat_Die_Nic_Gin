9e35647857e5fffff6094bb847bc5fde550b86e7
Nico Linder <nico.linder@noseryoung.ch>
2025-02-21 10:50:36 +0100

    snackautoamt changed

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
         window.setResizable(false);
-        window.setTitle("Snackautomat");
+        window.setTitle("Snack Automat");
         window.setBackground(Color. white);
 
         SnackPanel sp = new SnackPanel();
